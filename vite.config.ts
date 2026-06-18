c715175f0c1b7b39c8b6628c38bfa3343679fd37
rayandantas08-netizen <rayandantas08@gmail.com>
2026-06-18 14:25:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,17 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "./",  // ← Mude de "/professional-lawyer-website/" para "./"
-  plugins: [react(), tailwindcss()],
+  base: "/professional-lawyer-website/",
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  build: {
-    outDir: 'docs',
-    assetsDir: '.',
-    emptyOutDir: true,
-  }
 });
